93083f987d5fb94a7a3afc7981adb5152a42fd2f
Nushfa <fathimanushfa09@gmail.com>
2025-08-14 13:45:11 +0530

    Add .gitattributes to normalize line endings

---
+++ frontend/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div style={{
+      height: '100vh',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: '#000',
+      color: '#fff',
+      fontSize: '3rem',
+      fontFamily: 'sans-serif'
+    }}>
+      Coming Soon
+    </div>
+  );
+}
+
+export default App;
