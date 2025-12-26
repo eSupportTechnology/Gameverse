87dc50f2dc0cc07d6423f9591aa9f568d7ea3da8
Nushfa <fathimanushfa09@gmail.com>
2025-12-26 15:06:29 +0530

    update file name

---
+++ backend/app/Models/Booking.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Booking extends Model
+{
+
+    protected $fillable = [
+        'user_id',
+        'nfc_card_number',
+        'customer_name',
+        'phone_number',
+        'station',
+        'booking_date',
+        'start_time',
+        'duration',
+        'extended_time',
+        'payment_method',
+        'amount',
+        'status',
+        'end_time',
+        'vr_play'
+    ];
+
+
+    protected $casts = [
+        'booking_date' => 'date',
+        'amount' => 'decimal:2'
+    ];
+
+    //     public function station()
+    // {
+    //     return $this->belongsTo(\App\Models\Station::class);
+    // }    public function user()
+    public function user()
+    {
+        return $this->belongsTo(\App\Models\User::class);
+    }
+}
